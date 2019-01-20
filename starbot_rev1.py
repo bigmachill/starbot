2b0a3043d560dcf760fcb0a2cb5d94ecd3f97f1b
Nick <nmusienko@oakland.edu>
2019-01-19 23:58:04 -0500

    Added voice

--- starbot_rev1.py
+++ starbot_rev1.py
@@ -53,7 +53,105 @@ ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
                            pixel_order=ORDER)
 
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation, rotate=rotate or 0)
+print("Created device")
+
+def printMatrix(msg):
+    print(msg)
+    show_message(device, msg, fill="white", font=proportional(CP437_FONT))
+    time.sleep(1)
+
+def power_off_pi():
+    msg = 'Good bye!'
+    tts.say(msg)
+    printMatrix(msg)
+    subprocess.call('sudo shutdown now', shell=True)
 
 
+def reboot_pi():
+    msg = 'See you in a bit!'
+    tts.say(msg)
+    printMatrix(msg)
+    subprocess.call('sudo reboot', shell=True)
 
+
+def say_ip():
+    ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
+    msg = 'My IP address is %s' % ip_address.decode('utf-8')
+    tts.say(msg)
+    printMatrix(msg)
+    
+def led_on():
+    msg = 'Turning LED on'
+    tts.say(msg)
+    printMatrix(msg)
+    GPIO.output(26, GPIO.HIGH)
+    
+def led_off():
+    msg = 'Good bye!'
+    tts.say(msg)
+    printMatrix(msg)
+    tts.say('Turning LED off')
+    GPIO.output(26, GPIO.LOW)
+    
+def process_event(assistant, led, event):
+    logging.info(event)
+    if event.type == EventType.ON_START_FINISHED:
+        #led.state = Led.BEACON_DARK  # Ready.
+        printMatrix('Say "OK, Google" then speak, or press Ctrl+C to quit...')
+    elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+        #led.state = Led.ON  # Listening.
+    elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
+        print('You said:', event.args['text'])
+        text = event.args['text'].lower()
+        printMatrix(text)
+        if text == 'power off':
+            assistant.stop_conversation()
+            power_off_pi()
+        elif text == 'reboot':
+            assistant.stop_conversation()
+            reboot_pi()
+        elif text == 'ip address':
+            assistant.stop_conversation()
+            say_ip()
+        elif text == 'led on':
+            assistant.stop_conversation()
+            led_on()
+        elif text == 'led off':
+            assistant.stop_conversation()
+            led_off()
+        elif text == 'goodbye':
+            assistant.stop_conversation()
+            msg = 'See you later allegator!'
+            tts.say(msg)
+            printMatrix(msg)
+        elif text == 'see you later allegator!':
+            assistant.stop_conversation()
+            msg = 'In a while crocodile!'
+            tts.say(msg)
+            printMatrix(msg)
+            tts.say()
+            
+    elif event.type == EventType.ON_END_OF_UTTERANCE:
+        led.state = Led.PULSE_QUICK  # Thinking.
+    elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
+          or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
+          or event.type == EventType.ON_NO_RESPONSE):
+        led.state = Led.BEACON_DARK  # Ready.
+    elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
+        sys.exit(1)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    credentials = auth_helpers.get_assistant_credentials()
+    with Board() as board, Assistant(credentials) as assistant:
+        for event in assistant.start():
+            process_event(assistant, board.led, event)
+
+
+if __name__ == '__main__':
+    main()
 
