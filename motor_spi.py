7f577371918ebb066a1446a00304e32deae8ac35
kenzie <mckenzieking@oakland.edu>
2019-01-20 15:39:37 -0500

    Playing with motors

--- motor_spi.py
+++ motor_spi.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import sys
 import spidev
+import time
 
-rotate = int(sys.argv[1])
-cmd = rotate << 4
+cmd = int(sys.argv[1])
 spi = spidev.SpiDev()
 spi.open(0,1)
 spi.max_speed_hz = 8000000
-#spi.mode = 0b01
+spi.mode = 0b01
+spi.xfer2([cmd])
+time.sleep(0.1)
 spi.xfer2([cmd])
 spi.close()
