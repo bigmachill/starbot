5dbc44799b7e628897a2333d87c4fd76f2f662c5
Nick <nmusienko@oakland.edu>
2019-01-20 08:50:26 -0500

    Added voice and matrix functionality.

--- starbot_rev2.py
+++ starbot_rev2.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-
-#Assistant imports
 import logging
 import platform
 import subprocess
 import sys
-
 
 from google.assistant.library.event import EventType
 
@@ -14,11 +11,6 @@ from aiy.assistant import auth_helpers
 from aiy.assistant.library import Assistant
 from aiy.board import Board, Led
 from aiy.voice import tts
-
-#Neopixel imports
-import time
-import board
-import neopixel
 
 #Matrix imports
 import re
@@ -33,33 +25,27 @@ from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
-#Initializing Board
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-GPIO.setup(26, GPIO.OUT) #Servo 0
-GPIO.setup(6, GPIO.OUT) #Servo 1
-GPIO.setup(13, GPIO.OUT) #Servo 2
-GPIO.setup(5, GPIO.OUT) #Servo 3
-GPIO.setup(12, GPIO.OUT) #Servo 4
-GPIO.setup(24, GPIO.OUT) #Servo 5
-GPIO.setup(4, GPIO.OUT) #Driver 0
-GPIO.setup(17, GPIO.OUT) #Driver 1
-GPIO.setup(27, GPIO.OUT) #Driver 2
-GPIO.setup(22, GPIO.OUT) #Driver 3
-
-#Create Neopixel Strip for Head
-#NeoPixels must be connected to D10, D12, D18 or D21 to work.
-pixel_pin = board.D12
-num_pixels = 10
-ORDER = neopixel.GRB
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
-                           pixel_order=ORDER)
-
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90)
 print("Created device")
+
+
+#Initialize GPIO ports
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(26, GPIO.OUT) #Drivers 1
+GPIO.setup(6, GPIO.OUT) #Drivers 2
+GPIO.setup(13, GPIO.OUT) #Drivers 3
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+)
+
+def printMatrix(msg):
+    show_message(device, msg, fill="white", font=proportional(CP437_FONT), scroll_delay=0.017)
 
 
 def power_off_pi():
@@ -77,33 +63,27 @@ def say_ip():
     tts.say('My IP address is %s' % ip_address.decode('utf-8'))
     
 def led_on():
-    msg = 'Turning LED on'
-    tts.say(msg)
-    #printMatrix(msg)
-    pixels.fill((255, 0, 0))
-    
+    tts.say('Turning LED on')
+    printMatrix('Turning LED on')
+    GPIO.output(26, GPIO.HIGH)
     
 def led_off():
-    msg = 'Good bye!'
-    tts.say(msg)
-    #printMatrix(msg)
-    #tts.say('Turning LED off')
-    #GPIO.output(26, GPIO.LOW)
-    #pixels.fill((0, 0, 0))
-    #pixels.show()
-    time.sleep(1)
-    
+    tts.say('Turning LED off')
+    printMatrix('Turning LED off')
+    GPIO.output(26, GPIO.LOW)
+
+
 def process_event(assistant, led, event):
     logging.info(event)
     if event.type == EventType.ON_START_FINISHED:
-        led.state = Led.BEACON_DARK  # Ready.
-        #printMatrix('Say "OK, Google" then speak, or press Ctrl+C to quit...')
+        led.state = Led.BEACON_DARK  # Ready to accept commands.
+        print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
     elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
-        led.state = Led.ON  # Listening.
+        led.state = Led.ON  # Put code to demonstrate listening
     elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
         print('You said:', event.args['text'])
         text = event.args['text'].lower()
-        #printMatrix(text)
+        printMatrix(text)
         if text == 'power off':
             assistant.stop_conversation()
             power_off_pi()
@@ -116,25 +96,22 @@ def process_event(assistant, led, event):
         elif text == 'led on':
             assistant.stop_conversation()
             led_on()
-            pixels.show()
         elif text == 'led off':
             assistant.stop_conversation()
             led_off()
         elif text == 'goodbye':
             assistant.stop_conversation()
-            msg = 'See you later allegator!'
-            tts.say(msg)
-            printMatrix(msg)
+            tts.say('See you later allegator!')
         elif text == 'see you later allegator!':
             assistant.stop_conversation()
-            
+            tts.say('In a while crocodile!')
             
     elif event.type == EventType.ON_END_OF_UTTERANCE:
         led.state = Led.PULSE_QUICK  # Thinking.
     elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
           or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
           or event.type == EventType.ON_NO_RESPONSE):
-        led.state = Led.BEACON_DARK  # Ready.
+        led.state = Led.BEACON_DARK  # Ready to receive input.
     elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
         sys.exit(1)
 
@@ -150,4 +127,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
